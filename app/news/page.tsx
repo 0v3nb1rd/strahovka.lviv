cc4d1fcba245600c87e0f8d6cd8af19b1d443f1c
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-14 01:45:23 +0300

    add examples of posts

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -21,7 +21,7 @@ import Image from 'next/image'
 const NewsPage = () => {
   return (
     <main className="main main--news-cat">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto my-2 flex justify-between">
         {/* <h1 className="m-10 text-center text-4xl font-bold">News page</h1> */}
         <aside className="w-[220px]">
           <div className="flex flex-col gap-1">
@@ -37,9 +37,9 @@ const NewsPage = () => {
           </div>
         </aside>
 
-        <section className="">
+        <section className="mx-3 w-[640px]">
           {/* <Link href="/news/info-about-some-historical">Info about some historical</Link> */}
-          <article className="w-[640px] rounded-2xl bg-white">
+          <article className="rounded-2xl bg-white">
             <div className="mb-7">
               <div className="px-5 py-4">
                 <ul className="header mb-3 flex gap-6">
@@ -55,7 +55,7 @@ const NewsPage = () => {
                   <li className="ml-auto flex items-center gap-2">
                     <BiShow fontSize={22} />
                     <span className="countdown w-5 text-base">
-                      <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                      <span style={{ '--value': 3 } as React.CSSProperties}></span>
                     </span>
                   </li>
                 </ul>
@@ -83,6 +83,108 @@ const NewsPage = () => {
                 </button>
                 <button className="group ml-auto flex items-center gap-2">
                   <BiLike fontSize={22} className="text-red-500 transition group-hover:scale-110" />
+                  <span className="countdown w-5 text-base">
+                    <span style={{ '--value': 2 } as React.CSSProperties}></span>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </article>
+
+          <article className="rounded-2xl bg-white">
+            <div className="mb-7">
+              <div className="px-5 py-4">
+                <ul className="header mb-3 flex gap-6">
+                  <li className="">
+                    <a href="#" className="badge-secondary badge badge-lg hover:scale-110 hover:shadow-inner">
+                      Новини
+                    </a>
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <BiTimeFive />
+                    <span className="countdown text-base">10.12.2022</span>
+                  </li>
+                  <li className="ml-auto flex items-center gap-2">
+                    <BiShow fontSize={22} />
+                    <span className="countdown w-5 text-base">
+                      <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                    </span>
+                  </li>
+                </ul>
+                <h2 className="text-2xl font-medium">
+                  Автомобілістам росії доведеться замінити всі поліси ОСАЦВ з 1 липня 2016 року
+                </h2>
+              </div>
+
+              <figure className="h- relative h-[360px] w-full">
+                <Image src={img_post_2} alt="photo art 1" fill className="object-cover" />
+              </figure>
+
+              <div className="flex gap-10 px-5 py-4">
+                <button className="group flex items-center gap-2 transition">
+                  <BiMessageAltDetail fontSize={22} className="transition group-hover:scale-110" />
+                  <span className="countdown w-5 text-base">
+                    <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                  </span>
+                </button>
+                <button className="group">
+                  <BiBookmark fontSize={22} className="transition group-hover:scale-110" />
+                </button>
+                <button className="group">
+                  <BiShare fontSize={22} className="transition group-hover:scale-110" />
+                </button>
+                <button className="group ml-auto flex items-center gap-2">
+                  <BiLike fontSize={22} className=" transition group-hover:scale-110" />
+                  <span className="countdown w-5 text-base">
+                    <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </article>
+
+          <article className="rounded-2xl bg-white">
+            <div className="mb-7">
+              <div className="px-5 py-4">
+                <ul className="header mb-3 flex gap-6">
+                  <li className="">
+                    <a href="#" className="badge-secondary badge badge-lg hover:scale-110 hover:shadow-inner">
+                      Новини
+                    </a>
+                  </li>
+                  <li className="flex items-center gap-2">
+                    <BiTimeFive />
+                    <span className="countdown text-base">10.12.2022</span>
+                  </li>
+                  <li className="ml-auto flex items-center gap-2">
+                    <BiShow fontSize={22} />
+                    <span className="countdown w-5 text-base">
+                      <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                    </span>
+                  </li>
+                </ul>
+                <h2 className="text-2xl font-medium">Ліміти по ОСАЦВ в Україні будуть приведені до стандартів ЄС</h2>
+              </div>
+
+              <figure className="h- relative h-[360px] w-full">
+                <Image src={img_post_3} alt="photo art 1" fill className="object-cover" />
+              </figure>
+
+              <div className="flex gap-10 px-5 py-4">
+                <button className="group flex items-center gap-2 transition">
+                  <BiMessageAltDetail fontSize={22} className="transition group-hover:scale-110" />
+                  <span className="countdown w-5 text-base">
+                    <span style={{ '--value': 1 } as React.CSSProperties}></span>
+                  </span>
+                </button>
+                <button className="group">
+                  <BiBookmark fontSize={22} className="transition group-hover:scale-110" />
+                </button>
+                <button className="group">
+                  <BiShare fontSize={22} className="transition group-hover:scale-110" />
+                </button>
+                <button className="group ml-auto flex items-center gap-2">
+                  <BiLike fontSize={22} className=" transition group-hover:scale-110" />
                   <span className="countdown w-5 text-base">
                     <span style={{ '--value': 1 } as React.CSSProperties}></span>
                   </span>
@@ -115,7 +217,7 @@ const NewsPage = () => {
                   <div className="flex flex-1 flex-col gap-1">
                     <span className="text-xs text-neutral-700">Новини</span>
                     <h3 className="line-clamp-3 text-sm font-semibold transition group-hover:text-primary group-hover:underline">
-                      Автомобілістам росії доведеться замінити всі поліси ОСАЦВ з 1 липня 2016 року{' '}
+                      Автомобілістам росії доведеться замінити всі поліси ОСАЦВ з 1 липня 2016 року
                     </h3>
                   </div>
                   <figure className="relative h-14 w-10 overflow-hidden rounded-lg transition  group-hover:shadow">
@@ -129,7 +231,7 @@ const NewsPage = () => {
                   <div className="flex flex-1 flex-col gap-1">
                     <span className="text-xs text-neutral-700">Новини</span>
                     <h3 className="line-clamp-3 text-sm font-semibold transition group-hover:text-primary group-hover:underline">
-                      Ліміти по ОСАЦВ в Україні будуть приведені до стандартів ЄС{' '}
+                      Ліміти по ОСАЦВ в Україні будуть приведені до стандартів ЄС
                     </h3>
                   </div>
                   <figure className="relative h-14 w-10 overflow-hidden rounded-lg transition  group-hover:shadow">
