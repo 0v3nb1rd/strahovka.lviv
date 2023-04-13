a1e6981af398e7c468bad7dc8caf055f2cbbdf1c
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-14 00:57:13 +0300

    Blog: add right aside

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -8,6 +8,7 @@ import {
   BiInfoCircle,
   BiHome,
   BiBookmark,
+  BiNews,
   BiTimeFive,
 } from 'react-icons/Bi'
 
@@ -22,16 +23,16 @@ const NewsPage = () => {
     <main className="main main--news-cat">
       <div className="container mx-auto flex justify-between">
         {/* <h1 className="m-10 text-center text-4xl font-bold">News page</h1> */}
-        <aside>
-          <div className="flex flex-col">
-            <a className="btn-ghost btn gap-2 normal-case	" href="/news/">
-              <BiHome /> Усі статті
+        <aside className="w-[220px]">
+          <div className="flex flex-col gap-1">
+            <a className="btn-ghost btn-active btn w-[200px] justify-start gap-2 text-base normal-case" href="/news/">
+              <BiHome fontSize={24} /> Усі статті
             </a>
-            <a className="btn-ghost btn gap-2	normal-case" href="/news/">
-              <BiHome /> Новини
+            <a className="btn-ghost btn w-[200px] justify-start gap-2 text-base	normal-case" href="/news/">
+              <BiNews fontSize={24} /> Новини
             </a>
-            <a className="btn-ghost btn gap-2 normal-case	" href="/news/">
-              <BiInfoCircle /> Поради
+            <a className="btn-ghost btn w-[200px] justify-start gap-2 text-base normal-case	" href="/news/">
+              <BiInfoCircle fontSize={24} /> Поради
             </a>
           </div>
         </aside>
@@ -91,7 +92,54 @@ const NewsPage = () => {
           </article>
         </section>
 
-        <section className=""></section>
+        <section className="w-[320px]">
+          <div className="overflow-hidden rounded-2xl bg-white p-5">
+            <h2 className="text-lg font-bold">Популярні статті:</h2>
+            <ul>
+              <li>
+                <a href="#" className="group mt-4 flex items-center justify-between gap-2">
+                  <div className="flex flex-1 flex-col gap-1">
+                    <span className="text-xs text-neutral-700">Новини</span>
+                    <h3 className="line-clamp-3 text-sm font-semibold transition group-hover:text-primary group-hover:underline">
+                      В Україні в 2016 році потрібно ввести медстрахування всіх громадян — прем’єр
+                    </h3>
+                  </div>
+                  <figure className="relative h-14 w-10 overflow-hidden rounded-lg transition  group-hover:shadow">
+                    <Image src={img_post_1} alt="photo art 1" fill className="object-cover" />
+                  </figure>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" className="group mt-4 flex items-center justify-between gap-2">
+                  <div className="flex flex-1 flex-col gap-1">
+                    <span className="text-xs text-neutral-700">Новини</span>
+                    <h3 className="line-clamp-3 text-sm font-semibold transition group-hover:text-primary group-hover:underline">
+                      Автомобілістам росії доведеться замінити всі поліси ОСАЦВ з 1 липня 2016 року{' '}
+                    </h3>
+                  </div>
+                  <figure className="relative h-14 w-10 overflow-hidden rounded-lg transition  group-hover:shadow">
+                    <Image src={img_post_2} alt="photo art 1" fill className="object-cover" />
+                  </figure>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" className="group mt-4 flex items-center justify-between gap-2">
+                  <div className="flex flex-1 flex-col gap-1">
+                    <span className="text-xs text-neutral-700">Новини</span>
+                    <h3 className="line-clamp-3 text-sm font-semibold transition group-hover:text-primary group-hover:underline">
+                      Ліміти по ОСАЦВ в Україні будуть приведені до стандартів ЄС{' '}
+                    </h3>
+                  </div>
+                  <figure className="relative h-14 w-10 overflow-hidden rounded-lg transition  group-hover:shadow">
+                    <Image src={img_post_3} alt="photo art 1" fill className="object-cover" />
+                  </figure>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
       </div>
     </main>
   )
