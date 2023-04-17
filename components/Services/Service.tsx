dc67af50fb63998cce15556f1ee5d2a4def41727
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-17 14:34:30 +0300

    *

--- components/Services/Service.tsx
+++ components/Services/Service.tsx
@@ -8,20 +8,6 @@ interface ServiceProps {
   imgUrl: StaticImageData
 }
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#ffff" offset="20%" />
-      <stop stop-color="#ffff" offset="50%" />
-      <stop stop-color="#ffff" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#ffff" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
 const toBase64 = (str: string) =>
   typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str)
 
@@ -48,8 +34,6 @@ const Service: React.FC<ServiceProps> = ({ title, category, description, imgUrl 
               (max-width: 1200px) 50vw,
               33vw"
             alt="image cooperation"
-            placeholder="blur"
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           />
         </div>
       </div>
