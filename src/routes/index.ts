474f93c46aacb48784c412e7f7d5f1aa72620e97
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-18 11:52:10 +0300

    ✅ add routes

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+export const mainRoute = [
+  {
+    label: 'Новини',
+    path: '/news',
+  },
+  {
+    label: 'Послуги',
+    path: '/services',
+  },
+  {
+    label: 'Співпраця',
+    path: '/cooperation',
+  },
+  {
+    label: 'Контакти',
+    path: '/contacts',
+  },
+]
