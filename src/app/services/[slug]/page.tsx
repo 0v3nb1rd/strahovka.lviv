06b78ded5c917af41e543ee067bfae6cebbe96d8
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-26 18:23:50 +0300

    *

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -20,16 +20,17 @@ const fetchServices = async () => {
   return servicesData
 }
 
-const NewPage = async ({ params: { slug } }: paramsProps) => {
+const NewPage = async ({ params }: { params: any }) => {
   const servicesData = await fetchServices()
 
-  const { category_ua, title, full_text, img_url }: any = servicesData.find((x: any) => x.slug === slug)
+  const { category_ua, title, full_text, img_url }: any = servicesData.find((x: any) => x.slug === params.slug)
 
   return (
     <main className="main main--services pt-32">
       <div className="container mx-auto">
         <section className="mx-10 mb-14 mt-2 rounded-2xl  bg-white p-14 pb-20 shadow-md">
-          <div className="flex flex-col gap-16">
+          <h1>{servicesData[0].title}</h1>
+          {/* <div className="flex flex-col gap-16">
             <div className="title mx-auto max-w-2xl">
               <div className="flex flex-col items-center">
                 <span className="badge badge-secondary badge-lg mb-3">{category_ua}</span>
@@ -78,104 +79,7 @@ const NewPage = async ({ params: { slug } }: paramsProps) => {
               </ul>
               <b className="pl-14">Раді співпраці!</b>
             </div>
-
-            {/* <div className="flex justify-around">
-              <ul className="title flex w-[40%] flex-col gap-12">
-                <li>
-                  <div className="flex items-center gap-6">
-                    <div className="flex h-[72px] w-[72px] items-center justify-center rounded-2xl  bg-primary text-white shadow-2xl shadow-primary">
-                      <BiPhoneCall fontSize={34} />
-                    </div>
-                    <span className="flex flex-col">
-                      <span className="font-medium text-black/40">Телефон:</span>
-                      <a className=" font-bold leading-6">+38(068)767 88 98</a>
-                      <a className=" font-bold leading-6">+38(093)655 58 99</a>
-                    </span>
-                  </div>
-                </li>
-
-                <li>
-                  <div className="flex items-center gap-6">
-                    <div className="flex h-[72px] w-[72px] items-center justify-center rounded-2xl bg-[#80ed99] shadow-2xl shadow-[#80ed99]">
-                      <BiEnvelope fontSize={34} />
-                    </div>
-                    <span className="flex flex-col">
-                      <span className="font-medium text-black/40">Email:</span>
-                      <a className="text-lg font-bold leading-6">strahovka.lviv.ua@gmail.com</a>
-                    </span>
-                  </div>
-                </li>
-
-                <li>
-                  <div className="flex items-center gap-6">
-                    <div className="flex h-[72px] w-[72px] items-center justify-center rounded-2xl bg-secondary shadow-2xl shadow-secondary  ">
-                      <BiMap fontSize={34} />
-                    </div>
-                    <span className="flex flex-col">
-                      <span className="text-lg font-medium text-black/40">Адрес:</span>
-                      <span className="text-lg font-bold leading-6">
-                        м.Львів, 79000 <br /> пр.Шевченка
-                      </span>
-                    </span>
-                  </div>
-                </li>
-              </ul>
-
-              <form className="w-[40%]">
-                <div className="flex flex-col gap-6">
-                  <div className="relative z-0">
-                    <input
-                      type="text"
-                      name="name"
-                      className="text-md peer block w-full appearance-none border-0 border-b border-gray-400 bg-transparent px-2 py-2.5 text-gray-900 focus:border-accent focus:outline-none focus:ring-0"
-                      placeholder=" "
-                    />
-                    <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform pl-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-primary">
-                      Ваше ім'я *
-                    </label>
-                  </div>
-
-                  <div className="relative z-0">
-                    <input
-                      type="text"
-                      name="tel"
-                      className="text-md peer block w-full appearance-none border-0 border-b border-gray-400 bg-transparent px-2 py-2.5 text-gray-900 focus:border-accent focus:outline-none focus:ring-0"
-                      placeholder=" "
-                    />
-                    <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform pl-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-primary">
-                      Телефон *
-                    </label>
-                  </div>
-
-                  <div className="relative z-0">
-                    <input
-                      type="text"
-                      name="email"
-                      className="text-md peer block w-full appearance-none border-0 border-b border-gray-400 bg-transparent px-2 py-2.5 text-gray-900 focus:border-accent focus:outline-none focus:ring-0"
-                      placeholder=" "
-                    />
-                    <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform pl-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-primary">
-                      Email
-                    </label>
-                  </div>
-
-                  <div className="relative z-0">
-                    <textarea
-                      name="message"
-                      className="text-md peer block	h-24 w-full resize-none appearance-none border-0 border-b border-gray-400 bg-transparent px-2 py-2.5 text-gray-900 focus:border-accent focus:outline-none focus:ring-0"
-                      placeholder=" "
-                    ></textarea>
-                    <label className="absolute top-3 -z-10  origin-[0] -translate-y-6 scale-75 transform pl-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-primary peer-focus:dark:text-primary">
-                      Ваше повідомлення *
-                    </label>
-                  </div>
-                </div>
-                <button type="submit" className="btn-secondary btn-wide btn mt-8">
-                  Відправити
-                </button>
-              </form>
-            </div> */}
-          </div>
+          </div> */}
         </section>
       </div>
     </main>
