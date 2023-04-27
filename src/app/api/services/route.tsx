b39c9a5b90348cb4414b86786c04c0e7a84694a2
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-27 19:25:52 +0300

    ♻️ refactor code

--- src/app/api/services/route.tsx
+++ src/app/api/services/route.tsx
@@ -1,8 +1,15 @@
 import { NextResponse } from 'next/server'
-// import { v4 as uuid } from 'uuid'
 
 import serviceData from './serviceData.json'
 
 export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url)
+  const slug = searchParams.get('slug')
+
+  if (slug) {
+    const serviceItem = serviceData.find((item) => item.slug === slug)
+    return NextResponse.json(serviceItem)
+  }
+
   return NextResponse.json(serviceData)
 }
