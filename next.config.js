8f6204b72493037fa3450eb8384876e7f97e8be8
0v3nb1rd <0v3nb1rd@gmail.com>
2023-04-30 14:20:03 +0300

    *

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
   // images: {
   //   domains: ['pbs.twimg.com', 'picsum.photos'],
   // },
-  // i18n: {
-  //   localeDetection: false,
-  //   locales: ['uk', 'en'],
-  //   defaultLocale: 'uk',
-  // },
+  i18n: {
+    localeDetection: false,
+    locales: ['uk', 'en'],
+    defaultLocale: 'uk',
+  },
 }
 
 module.exports = nextConfig
