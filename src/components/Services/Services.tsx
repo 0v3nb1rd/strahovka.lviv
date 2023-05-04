b913f04fddb4c063aa373f0dcade5702d5419baf
0v3nb1rd <0v3nb1rd@gmail.com>
2023-05-04 23:47:30 +0300

    upd

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,19 +1,21 @@
 import Service from './Service'
 
-const Services = ({ data }: any) => (
-  <ul className="grid grid-cols-3 gap-8">
-    {data.map((service: any) => (
-      <li key={service.id}>
-        <Service
-          title={service.title}
-          category={service.category_ua}
-          description={service.short_text}
-          imgUrl={service.thumbnail_url}
-          slug={service.slug}
-        />
-      </li>
-    ))}
-  </ul>
-)
+const Services = ({ data }: any) => {
+  return (
+    <ul className="grid grid-cols-3 gap-8">
+      {data?.map((service: any) => (
+        <li key={service.id}>
+          <Service
+            title={service.title}
+            category={service.category_ua}
+            description={service.short_text}
+            imgUrl={service.thumbnail_url}
+            slug={service.slug}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default Services
