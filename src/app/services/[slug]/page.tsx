d0d0c6fdf9783bdcc081cb8004dddb0a20849a94
0v3nb1rd <0v3nb1rd@gmail.com>
2023-05-17 17:58:06 +0300

    add: back button

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -1,12 +1,13 @@
-import { PrismaClient, Service_category } from '@prisma/client'
+import { Service_category } from '@prisma/client'
+
+import prisma from '@/lib/prisma'
 
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface Props {
   params: { slug: string }
 }
-
-const prisma = new PrismaClient()
 
 const fetchServiceBySlug = async (slug: string): Promise<Service_category | null> => {
   const res = await prisma.service_category.findFirst({ where: { slug: slug } })
@@ -19,11 +20,25 @@ export default async function NewPage({ params }: Props) {
   const service = await fetchServiceBySlug(params.slug)
 
   return (
-    <section className="mx-10 rounded-2xl bg-white p-14 pb-20 shadow-md">
+    <section className="relative mx-10 rounded-2xl bg-white p-14 pb-20 shadow-md">
+      <Link
+        href="/services"
+        className="btn-outline btn-circle btn absolute right-4 top-4 text-primary hover:border-transparent hover:bg-primary"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </Link>
       <div className="flex flex-col gap-8">
         <div className="title mx-auto max-w-2xl">
           <div className="flex flex-col items-center">
-            <span className="badge-secondary badge badge-lg mb-3">{service?.category_ua}</span>
+            <span className="badge badge-secondary badge-lg mb-3">{service?.category_ua}</span>
             <h1 className="mb-2 max-w-2xl text-center text-2xl font-bold leading-tight sm:text-3xl md:text-4xl md:leading-tight">
               {service?.title}
             </h1>
