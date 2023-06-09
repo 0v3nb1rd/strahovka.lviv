4f1b359020a7494fbddd3ff136d859f70d0cf44a
0v3nb1rd <0v3nb1rd@gmail.com>
2023-06-09 19:59:36 +0300

    add server actions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Service_category } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
@@ -9,3 +9,26 @@ const prisma = global.prisma || new PrismaClient()
 if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
+
+////////////////////////////////////////////////////////////////////////
+//! Server actions:
+////////////////////////////////////////////////////////////////////////
+
+export async function getServices(): Promise<Service_category[] | void> {
+  try {
+    const res = await prisma.service_category.findMany()
+    return res
+  } catch (error) {
+    return console.log(error)
+  }
+}
+
+export async function createService(title: string) {
+  try {
+  } catch (error) {}
+}
+
+export async function updateService() {
+  try {
+  } catch (error) {}
+}
