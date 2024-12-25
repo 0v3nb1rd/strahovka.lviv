6662e24647016b3b8dffde100db4ecf384c8b3c5
0v3nb1rd <0v3nb1rd@gmail.com>
2024-12-25 12:58:56 +0100

    Update layout component with enhanced metadata and improved structure
    - Refactored site metadata for better clarity and SEO, including updated title and description.
    - Added support for Apple Web App configuration and favicon icons.
    - Included React import for proper component functionality.
    - Adjusted HTML language attribute for consistency.

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import { Header, Footer, Drawer } from '@/components'
 
 import '@/styles/globals.scss'
+import React from 'react'
 
 type propType = {
   children: React.ReactNode
@@ -8,14 +9,24 @@ type propType = {
 }
 
 export const metadata = {
-  title: 'Страхування у Львові та Україні від кращих компаній',
+  title: "Страхування в Україні | Страхування Львів | Страхування від кращих компаній",
   description:
-    "Послуги Страхування у Львові та Україні | Автострахування Страхування майна Страхування здоров'я Калькулятор розрахунку ціни тел 068 767 88 98; 093 655 58 99",
+    "Послуги Страхування Львів та Україна | Автострахування Страхування майна Страхування здоров'я Калькулятор розрахунку ціни тел 068 767 88 98; 063 744 12 89",
+  appleWebApp: {
+    title: 'Strahovka',
+    statusBarStyle: 'default',
+  },
+  icons: {
+    icon: '/favicons/favicon.ico',
+    apple: [
+      { url: '/favicons/apple-icon.png' },
+    ],
+  },
 }
 
 export default function RootLayout(props: propType) {
   return (
-    <html lang="ua">
+    <html lang={`ua`}>
       <body>
         <div className="drawer absolute">
           <input id="drawer-menu" type="checkbox" className="drawer-toggle" />
